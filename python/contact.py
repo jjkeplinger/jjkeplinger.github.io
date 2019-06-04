f8da10a27a43915280cdda5f45725340cbfb36f9
Jennifer Keplinger <jenniferkeplinger@Jennifers-MacBook-Pro-2.local>
2019-06-04 14:37:03 -0400

    test

--- python/contact.py
+++ python/contact.py
@@ -1,12 +1,12 @@
 contact_info = {
-"first": "Jennifer",
+"first": "Jennifer ",
 "last": "Keplinger",
 "street": "102 South Cleveland Avenue",
-"city": "Galien",
-"state": "MI",
+"city": "Galien, ",
+"state": "MI ",
 "zip": "49113",
 }
 
-print(contact_info['first'] + ' ' + ['last'])
+print(contact_info['first'] + ['last'])
 print(contact_info['street'])
-print(contact_info['city'] + ', ' + ['state'] + ' ' + ['zip'])
+print(contact_info['city'] + ['state'] + ['zip'])
