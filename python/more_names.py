8e9a6d626ea0f80d23539f5b55f26e33b87e8de8
Jennifer Keplinger <jenniferkeplinger@Jennifers-MacBook-Pro-2.local>
2019-06-04 16:40:01 -0400

    jafo

--- python/more_names.py
+++ python/more_names.py
@@ -3,3 +3,8 @@ hilt_class = ["Jennifer", "Sally", "Sam", "Mary"]
 for name in hilt_class:
     if name == "Sally":
             print(name)
+
+states = {"MI": "Michigan", "ME": "Maine"}
+
+for code, state in states.items():
+    print(code + "is the code for " + state)
