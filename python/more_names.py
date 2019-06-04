83c026b045a4dde4092198b5b47498e0fa9b544d
Jennifer Keplinger <jenniferkeplinger@Jennifers-MacBook-Pro-2.local>
2019-06-04 16:47:14 -0400

    fmdk

--- python/more_names.py
+++ python/more_names.py
@@ -8,3 +8,12 @@ states = {"MI": "Michigan", "ME": "Maine"}
 
 for code, state in states.items():
     print(code + " is the code for " + state)
+
+for i in range(10):
+    print("I am at number " + str(i))
+
+for i in range(5, 10):
+    print("I'm at number " + str(i))
+
+for i in range(0, 10, 12):
+    print("I'm at number " + str(i))
