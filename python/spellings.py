e597a360f849ef82c3f5016ff002dda75ea8c763
Jennifer Keplinger <jenniferkeplinger@Jennifers-MacBook-Pro-2.local>
2019-06-05 11:32:44 -0400

    t

--- python/spellings.py
+++ python/spellings.py
@@ -2,5 +2,5 @@ words = ["color", "colour", "gray", "grey"]
 
 correct_spelling = ["color", "gray"]
 
-if words == color or colour
-    print("color")
+if words == words[1] or words[2]
+    print(words[1])
