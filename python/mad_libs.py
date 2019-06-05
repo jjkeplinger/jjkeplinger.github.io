49981ba673ea02b2eac5b4d9ae25de0732c4fbcf
Jennifer Keplinger <jenniferkeplinger@Jennifers-MacBook-Pro-2.local>
2019-06-05 10:16:21 -0400

    test

--- python/mad_libs.py
+++ python/mad_libs.py
@@ -5,3 +5,11 @@ places = ["store", "beach", "ice cream parlor", "microbrewery"]
 
 for place in places:
 	mad_lib(place)
+
+def mad_lib(companion):
+    print(place + "with " + companion)
+
+companions = ["Viola", "Jette", "Frank"]
+
+for companion in companions:
+    mad_lib(companion)
