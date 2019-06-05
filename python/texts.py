9a539b2100b677c8c90633b8c6fdbce5ad0fb0d6
Jennifer Keplinger <jenniferkeplinger@Jennifers-MacBook-Pro-2.local>
2019-06-05 11:12:22 -0400

    t

--- python/texts.py
+++ python/texts.py
@@ -1,6 +1,9 @@
 texts = {
-"Jane Eyre": "1847", "Cane": "1923", "Wide Sargasso Sea": "1966", "Citizen: An American Lyric": "2014"
+    "Jane Eyre": "1847",
+    "Cane": "1923",
+    "Wide Sargasso Sea": "1966",
+    "Citizen: An American Lyric": "2014"
 }
 
-for title, date in texts.items()
+for title, date in texts.items():
     print(title + " was published in " + date + ".")
